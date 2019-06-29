34c67f810bba83c323767a65cb98818b2134bfe1
Shanmukh Koka <saikoka.sk@gmail.com>
2019-06-29 02:08:34 +0000

    Added mailing functionality

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer<ActionMailer::base
+    default to: 'saikoka.sk@gmail.com'
+    
+    def contact_email(name, email, body)
+        @name=name
+        @email=email
+        @body=body
+        mail(from: email, subject: 'Contact Form Message')
+    end 
+end
