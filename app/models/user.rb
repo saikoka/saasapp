ba35b613dbae18fd914180e877486996ae5d29e5
Shanmukh Koka <saikoka.sk@gmail.com>
2019-07-05 18:54:55 +0000

    Added subscription service

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   attr_accessor :stripe_card_token
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: email, plan:plan_id, card: stripe_card_token)
-      self.stripe_customer_token=customer.id
-      save!
+    customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+    self.stripe_customer_token = customer.id
+    save!
     end
   end
 end
