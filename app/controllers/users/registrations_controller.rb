eb9c927712b34a3dd466369a004c24d7a72e2003
Shanmukh Koka <saikoka.sk@gmail.com>
2019-07-05 19:35:28 +0000

    Changed flash messages

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :select_plan, only: :new
+  
+  #allows for pro users to be saved to database with subscription function
   def create
     super do |resource|
       if params[:plan]
@@ -11,4 +14,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+  
+  
+  private 
+    def select_plan
+      unless (params[:plan] =='1'||params[:plan] == '2')
+      flash[:notice] = "please select a membership plan."
+      redirect_to root_url
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   belongs_to :plan
   
   attr_accessor :stripe_card_token
+  # If information is valid, call stripe to setup subscription upon charging card.
+  # Likely where my error was?
   def save_with_subscription
     if valid?
     customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
